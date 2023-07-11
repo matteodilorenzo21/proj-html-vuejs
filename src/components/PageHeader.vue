104bed22cfcb806d1837e2c1c304cfe528ccb607
matteodilorenzo21 <matteodilorenzo21@gmail.com>
2023-07-11 10:03:29 +0200

    first commit

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+};
+</script>
+
+<template>
+ 
+</template>
+
+<style lang="scss" scoped>
+@import './assets/scss/style.scss';
+@import './assets/scss/_vars.scss';
+
+</style>
